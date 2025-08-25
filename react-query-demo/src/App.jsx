503f3f3161b4fdbf646532581c93f28dcbb6f123
Ann David <annapauladav@gmail.com>
2025-08-25 01:50:01 -0700

    Update file

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import PostsComponent from "./components/PostsComponent";
+
+// create the client inside App.jsx
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div style={{ padding: "20px" }}>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-const queryClient = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
-)
+);
