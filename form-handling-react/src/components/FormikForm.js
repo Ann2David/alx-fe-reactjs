d3e7bea18c11398db9e1f224a93cfb8d58486ebb
Ann David <annapauladav@gmail.com>
2025-08-25 01:14:17 -0700

    Add new changes

--- form-handling-react/src/components/FormikForm.js
+++ form-handling-react/src/components/FormikForm.js
@@ -12,4 +12,37 @@ const validationSchema = Yup.object({
 const FormikForm = () => {
   return (
     <Formik
-      initialValues={{ username: "", email: "",
+      initialValues={{ username: "", email: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        console.log("Formik form submitted:", values);
+      }}
+    >
+      {() => (
+        <Form>
+          <div>
+            <label>Username:</label>
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="p" style={{ color: "red" }} />
+          </div>
+
+          <div>
+            <label>Email:</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="p" style={{ color: "red" }} />
+          </div>
+
+          <div>
+            <label>Password:</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="p" style={{ color: "red" }} />
+          </div>
+
+          <button type="submit">Register</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default FormikForm;
