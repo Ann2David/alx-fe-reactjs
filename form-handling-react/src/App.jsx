be40ea3d55227892693c2466b7c1a78924f8d021
Ann David <annapauladav@gmail.com>
2025-08-24 22:56:52 -0700

    Add RegistrationForm with controlled components and FormikForm

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -4,8 +4,9 @@ import FormikForm from "./components/FormikForm";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center space-y-10 p-4">
+    <div>
       <RegistrationForm />
+      <hr />
       <FormikForm />
     </div>
   );
